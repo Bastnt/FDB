49cd8c7b75c88cbaeed05604b83ac49b9d2929a9
Éole (Léo Guirinec) <filo.mystere@gmail.com>
2013-12-20 01:11:28 +0100

    Creates Verifier

--- fdb.py
+++ fdb.py
@@ -14,14 +14,12 @@ def execute(request):
 	#for c in s.findall("pokemon").children:
 	#	print(c.name)
 
-	ex = """for $var in doc("schema_federe.xml")//trainerName[. <> 'p']/../@id return $var"""
+	ex = """for $var in doc("schema_federe.xml")//trainerName[. <> 'p']/../@id where  $var > "b" or ($var=true and $var/../cul = "mignon")  return $var"""
 	simplified_request = verifier.verify(ex)
 	if(simplified_request == None):
 		return None
 
 	splitter.main(simplified_request)
-
-
 
 	# Tests
 	#print(xml_wrapper.execute(Req("type55", "", "moves")))
